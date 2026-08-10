54c9c5d9089cf03d3984956c76ede170640cb11e
Phuoc Anh <phuocanhha1742013@gmail.com>
2026-08-10 09:16:29 +0700

    Add template

---
+++ template/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+#define vi vector<int>
+#define vll vector<long long>
+
+#define pi pair<int, int>
+#define pll pair<ll, ll>
+
+#define mi pair<int, int>
+#define mll pair<ll, ll>
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+constexpr int MOD = 1e9 + 7;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    return 0;
+}
